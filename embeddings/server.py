137036f0933bf063897e731e7dbc6c251fbdc4b5
Shardul Nalegave <shardul.nalegave.2005@gmail.com>
2025-03-23 20:20:50 +0530

    Implemented embeddings service

--- embeddings/server.py
+++ embeddings/server.py
@@ -1,8 +1,51 @@
 
-from flask import Flask
+from flask import Flask, request
+from embeddings.specter2_proximity import Specter2Proximity
+from embeddings.specter2_ad_hoc_query import Specter2AdHocQuery
 
 app = Flask("embeddings")
+proximity_model = Specter2Proximity()
+adhoc_query_model = Specter2AdHocQuery()
 
 @app.route("/")
 def index():
     return "Hello, World!"
+
+@app.post("/generate/proximity")
+def get_proximity_embedding():
+    paper = request.get_json(silent=True)
+    if paper == None:
+        return {
+            'message': 'Could not parse request body'
+        }, 400
+
+    if paper.get('title') == None or paper.get('abstract') == None:
+        return {
+            'message': 'Required fields "title" and/or "abstract" not present in request body'
+        }, 400
+    
+    embedding = proximity_model.generate_embeddings(paper)
+    return {
+        'dims': len(embedding),
+        'embedding': embedding,
+    }
+
+@app.post("/generate/adhoc_query")
+def get_adhoc_query_embedding():
+    query = request.get_json(silent=True)
+    if query == None:
+        return {
+            'message': 'Could not parse request body'
+        }, 400
+
+    if query.get('query') == None:
+        return {
+            'message': 'Required field "query" not present in request body'
+        }, 400
+    
+    query = query['query']
+    embedding = adhoc_query_model.generate_embeddings(query)
+    return {
+        'dims': len(embedding),
+        'embedding': embedding,
+    }
---
+++ embeddings/specter2_ad_hoc_query.py
@@ -0,0 +1,29 @@
+
+import torch
+from transformers import AutoTokenizer
+from adapters import AutoAdapterModel
+
+class Specter2AdHocQuery:
+    def __init__(self):
+        self.tokenizer = AutoTokenizer.from_pretrained('allenai/specter2_base')
+        self.model = AutoAdapterModel.from_pretrained('allenai/specter2_base')
+        self.model.load_adapter("allenai/specter2_adhoc_query", source="hf", load_as="proximity", set_active=True)
+    
+    def generate_embeddings(self, query: str):
+        query = query.strip()
+
+        inputs = self.tokenizer(
+            query,
+            padding=True, 
+            truncation=True,
+            return_tensors="pt",
+            return_token_type_ids=False,
+            max_length=512
+        )
+        
+        with torch.no_grad():
+            output = self.model(**inputs)
+            embedding = output.last_hidden_state.mean(dim=1).squeeze()
+            embedding = embedding.cpu().numpy().tolist()
+
+        return embedding
